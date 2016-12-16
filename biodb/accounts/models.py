8ff406393e4b5d8a5c33d7ec960859b57d8cd128
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-16 15:19:51 +0100

    Fix 'change mail in profile' bug

--- biodb/accounts/models.py
+++ biodb/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User  
 from django.utils.translation import ugettext as _  
-from userena.models import UserenaBaseProfile  
+from userena.models import UserenaBaseProfile, UserenaSignup  
 
 # Create your models here.
 
@@ -9,3 +9,5 @@ class MyProfile(UserenaBaseProfile):
     user = models.OneToOneField(User,unique=True,  
                   verbose_name=_('user'),related_name='my_profile')  
     favourite_snack = models.CharField(_('favouritesnack'),max_length=5) 
+    # Automagically create the user UserenaSignup object when referenced
+    User.userena_signup = property(lambda u: UserenaSignup.objects.get_or_create(user=u)[0])
