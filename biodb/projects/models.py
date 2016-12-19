dd68e75e805b6d29377536ca9517f77adc7f19a7
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-19 15:16:52 +0100

    Add max_length attr to CharField in Project model

--- biodb/projects/models.py
+++ biodb/projects/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     create_date = models.DateField()
     description = models.TextField()
