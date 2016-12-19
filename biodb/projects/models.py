e39e97b5bb019a8d0daa8f5c8f8fec037abcba54
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-19 14:59:09 +0100

    Create Project model

--- biodb/projects/models.py
+++ biodb/projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField()
+    create_date = models.DateField()
+    description = models.TextField()
