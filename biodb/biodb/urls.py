fae55e8f3d023dab3acc96430b421cfcf28b3742
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-20 13:40:00 +0100

    Add namespace to projects urls

--- biodb/biodb/urls.py
+++ biodb/biodb/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+app_name = "biodb"
 urlpatterns = [
     # Examples:
     # url(r'^$', 'biodb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^projects/{0,1}', include('projects.urls')),
+    url(r'^projects/', include('projects.urls', namespace="projects")),
     url(r'^', include('userena.urls'))  
 ]
 
