50935cb7003bee94b5aa3f75fac39e2dedd4ad10
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-20 13:41:33 +0100

    Add new patterns to projects urlConf

--- biodb/projects/urls.py
+++ biodb/projects/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
-from views import IndexView
+from views import ProjectsView, ProjectDetailView, ProjectUpdateView
 
+app_name = "projects"
 urlpatterns = [
-    url(r'^', IndexView.as_view(), name="index")
+    url(r'^$', ProjectsView.as_view(), name="projects_list"),
+    url(r'^(?P<pk>[0-9]{1,3})/$', ProjectDetailView.as_view(), name = "project_detail"),
+    url(r'^(?P<pk>[0-9]{1,3})/edit/$', ProjectUpdateView.as_view(), name="project_update")
 ]
