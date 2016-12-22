976baff1aab3588a131c52a61e5da15dfa0c9cbc
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-22 15:53:48 +0100

    Add slug to project_detail url pattern

--- biodb/projects/urls.py
+++ biodb/projects/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
-from views import ProjectsView, ProjectDetailView, ProjectUpdateView
+from views import ProjectListView, ProjectDetailView, ProjectUpdateView
 
 app_name = "projects"
 urlpatterns = [
-    url(r'^$', ProjectsView.as_view(), name="projects_list"),
-    url(r'^(?P<pk>[0-9]{1,3})/$', ProjectDetailView.as_view(), name = "project_detail"),
+    url(r'^$', ProjectListView.as_view(), name="project_list"),
+    url(r'^(?P<slug>\w+)/$', ProjectDetailView.as_view(), name = "project_detail"),
     url(r'^(?P<pk>[0-9]{1,3})/edit/$', ProjectUpdateView.as_view(), name="project_update")
 ]
