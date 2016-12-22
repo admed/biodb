0de50a8a3086f26c53f3085e5140e26410002881
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-22 14:57:53 +0100

    Add new models.

--- biodb/projects/models.py
+++ biodb/projects/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from simple_history.models import HistoricalRecords
 
 # Create your models here.
 class Project(models.Model):
@@ -12,4 +14,48 @@ class Project(models.Model):
         )
 
     def __str__(self):
+<<<<<<< Updated upstream
         return self.name
+||||||| merged common ancestors
+        return self.name
+=======
+        return self.name
+
+class Name(models.Model):
+    title = models.CharField()
+    primary = models.CharField()
+
+class Tag(models.Model):
+    name = models.CharField()
+
+class MoleculeFile(models.Model):
+    file = models.FileField()
+    description = models.RichTextField()
+
+class BioObj(models.Model):
+    ligand = models.CharField()
+    receptor = models.CharField()
+    ref_seq = RichTextField()
+    mod_seq = RichTextField()
+    description = RichTextField()
+    bibliography = RichTextField()
+    ref_commercial = RichTextField()
+    ref_clinical = RichTextField()
+
+class RObject(models.Model):
+    names = models.ForeignKey(Name)
+    create_date = models.DateField()
+    history = HistoricalRecords()
+    author = models.CharField()
+    creator = models.CharField()
+    bio_obj = models.ForeignKey(BioObj)
+    # sample = models.ForeignKey(Sample)
+    notes = RichTextField()
+    tags = models.ForeignKey(Tag)
+    files = models.ForeignKey(MoleculeFile)
+
+
+
+
+
+>>>>>>> Stashed changes
