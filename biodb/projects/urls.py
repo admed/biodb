fc35dcdf128a57c1c4ae5579b49f2d0005415cfd
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-28 15:03:57 +0100

    Replace slug by project_name in project_detail pattern

--- biodb/projects/urls.py
+++ biodb/projects/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, url
-from views import ProjectListView, ProjectDetailView, ProjectUpdateView
+from django.conf.urls import url
+from views import ProjectListView, RObjectListView, ProjectUpdateView
 
 app_name = "projects"
 urlpatterns = [
     url(r'^$', ProjectListView.as_view(), name="project_list"),
-    url(r'^(?P<slug>\w+)/$', ProjectDetailView.as_view(), name = "project_detail"),
+    url(r'^(?P<project_name>\w+)/$', RObjectListView.as_view(), name = "project_detail"),
     url(r'^(?P<pk>[0-9]{1,3})/edit/$', ProjectUpdateView.as_view(), name="project_update")
 ]
