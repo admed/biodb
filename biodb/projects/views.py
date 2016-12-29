e6f117a58788393eb7019ec74f8acf40b2932f3e
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-29 12:47:45 +0100

    Use SimpleTableView inside RObjectListView

--- biodb/projects/views.py
+++ biodb/projects/views.py
@@ -1,12 +1,13 @@
 from biodb import settings
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import Group
 # from django.http import HttpResponse
 from mixins import ProjectPermissionMixin
 from django.views import generic
 from models import Project, RObject
 # from guardian.shortcuts import get_objects_for_user
 from guardian.mixins import PermissionRequiredMixin, PermissionListMixin, LoginRequiredMixin
+from tables import RObjectTable
+from django_tables2 import SingleTableView
 
 
 # Create your views here.
@@ -21,18 +22,16 @@ class ProjectListView(LoginRequiredMixin, PermissionListMixin, generic.ListView)
     model = Project
     context_object_name = "projects"
 
-class RObjectListView(ProjectPermissionMixin, PermissionRequiredMixin, generic.ListView):
+class RObjectListView(ProjectPermissionMixin, PermissionRequiredMixin, SingleTableView):
     permission_required = 'projects.can_visit_project'
-    template_name = 'projects/RObject_list.html'
+    template_name = 'projects/robject_list.html'
     model = RObject
-    context_object_name = "robjects"
     raise_exception = True
-    # slug_field = "name"
+    table_class = RObjectTable
 
     def get_queryset(self, *args, **kwargs):
         project = self.get_permission_object()
         return project.robject_set.all()
-
 
 
 class ProjectUpdateView(PermissionRequiredMixin, generic.UpdateView):
