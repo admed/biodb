e37fa30dd28132e4830e1611c2776141d26f9423
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-29 11:14:42 +0100

    Temporary add Name model to admin

--- biodb/projects/admin.py
+++ biodb/projects/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from models import Project, RObject
+from models import Project, RObject, Name
 from guardian.admin import GuardedModelAdmin
 
 # Register your models here.
 admin.site.register(Project, GuardedModelAdmin)
 admin.site.register(RObject, GuardedModelAdmin)
+admin.site.register(Name, GuardedModelAdmin)
