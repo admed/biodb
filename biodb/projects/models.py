b72ca0308d848cf8a027a24b38709fa329efba5f
Mateusz <mateusz.kirmuc@gmail.com>
2016-12-29 11:16:19 +0100

    Add Name model and custom __str__ method to RObject

--- biodb/projects/models.py
+++ biodb/projects/models.py
@@ -7,6 +7,7 @@ from simple_history.models import HistoricalRecords
 
 class Project(models.Model):
     # it's slug field! letters, numbers, underscores or hyphens allowed only!
+    # TODO: consider using SlugField in this model and set on autopopulate (docs) 
     name = models.CharField(max_length=100, null=True)
     create_date = models.DateField(null=True)
     description = models.TextField(null=True)
@@ -19,9 +20,6 @@ class Project(models.Model):
     def __str__(self):
         return self.name
 
-class Name(models.Model):
-    title = models.CharField(max_length=100)
-    primary = models.CharField(max_length=100)
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
@@ -42,14 +40,26 @@ class BioObj(models.Model):
 
 class RObject(models.Model):
     project = models.ForeignKey(Project, null=True)
-    names = models.ForeignKey(Name, null=True, blank=True)
     create_date = models.DateField(null=True)
     history = HistoricalRecords()
     author = models.CharField(max_length=100, null=True)
     creator = models.CharField(max_length=100, null=True)
     bio_obj = models.ForeignKey(BioObj, null=True, blank=True)
-    # sample = models.ForeignKey(Sample)
     # notes = RichTextField() # FIXME admin site problems!
     tags = models.ForeignKey(Tag, null=True, blank=True)
     files = models.ForeignKey(MoleculeFile, null=True, blank=True)
 
+    def __str__(self):
+        if not self.name_set.all():
+            # create temporary, working title 
+            return "noname robject, created at {}, by {}".format(self.create_date, self.author)
+        else:
+            # get related Name model with primary=True
+            name = self.name_set.all().get(primary=True)
+            return name.title
+            
+
+class Name(models.Model):
+    title = models.CharField(max_length=100)
+    primary = models.BooleanField(blank=True)
+    robject = models.ForeignKey(RObject, null=True)
