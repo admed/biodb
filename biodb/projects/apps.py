0781244d3e6da8305d232713d4108e91599c9250
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-03 12:36:19 +0100

    Comment out content in projects/apps.py

--- biodb/projects/apps.py
+++ biodb/projects/apps.py
@@ -1,8 +1,8 @@
-from django.apps import AppConfig
-from watson import search as watson
+# from django.apps import AppConfig
+# from watson import search as watson
 
-class ProjectsConfig(AppConfig):
-    name = "projects"
-    def ready(self):
-        RObject = self.get_model("RObject")
-        watson.register(RObject)
+# class ProjectsConfig(AppConfig):
+#     name = "projects"
+#     def ready(self):
+#         RObject = self.get_model("RObject")
+#         watson.register(RObject, fields=("creator__username",))
