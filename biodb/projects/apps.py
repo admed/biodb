5611ca00006e818e7b41f5644027bf48bdade7ff
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-03 12:37:22 +0100

    Delete projects/apps.py

--- biodb/projects/apps.py
+++
@@ -1,8 +0,0 @@
-# from django.apps import AppConfig
-# from watson import search as watson
-
-# class ProjectsConfig(AppConfig):
-#     name = "projects"
-#     def ready(self):
-#         RObject = self.get_model("RObject")
-#         watson.register(RObject, fields=("creator__username",))
