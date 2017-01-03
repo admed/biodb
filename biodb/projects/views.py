835131f8f2ce7292f6e52f858f2c57df6ef548ca
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-03 12:53:22 +0100

    Enable searching in RobjectListView

--- biodb/projects/views.py
+++ biodb/projects/views.py
@@ -1,7 +1,7 @@
 from biodb import settings
 from django.shortcuts import render, redirect, get_object_or_404
 # from django.http import HttpResponse
-from mixins import ProjectPermissionMixin
+from mixins import ProjectPermissionMixin, SearchMixin
 from django.views import generic
 from models import Project, RObject
 # from guardian.shortcuts import get_objects_for_user
@@ -22,7 +22,7 @@ class ProjectListView(LoginRequiredMixin, PermissionListMixin, generic.ListView)
     model = Project
     context_object_name = "projects"
 
-class RObjectListView(ProjectPermissionMixin, PermissionRequiredMixin, SingleTableView):
+class RObjectListView(ProjectPermissionMixin, PermissionRequiredMixin, SingleTableView, SearchMixin):
     permission_required = 'projects.can_visit_project'
     template_name = 'projects/robject_list.html'
     model = RObject
@@ -30,9 +30,16 @@ class RObjectListView(ProjectPermissionMixin, PermissionRequiredMixin, SingleTab
     table_class = RObjectTable
 
     def get_queryset(self, *args, **kwargs):
-        project = self.get_permission_object()
-        return project.robject_set.all()
+        ''' Limit robjects to those related to project ''' 
 
+        project = self.get_permission_object()
+        queryset = project.robject_set.all()
+        query = self.request.GET.get("query", None)
+
+        if query:
+            queryset = self.search(queryset=queryset, query=query)
+        
+        return queryset
 
 class ProjectUpdateView(PermissionRequiredMixin, generic.UpdateView):
     permission_required = 'projects.change_project'
