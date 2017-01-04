c547ed4d00eba46346d638719b8062ca648b67e1
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-04 10:09:24 +0100

    Create SearchFilterForm

---
+++ biodb/projects/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class SearchFilterForm(forms.Form):
+
+    phrase = forms.CharField(label=False, required=False, widget=forms.TextInput(
+        attrs={'placeholder': 'search phrase','class': 'search-bar', 'name':'query'}))
+    after_date = forms.DateField(label=False, required=False, widget=forms.DateInput(
+        attrs={'placeholder': 'date after', "class": "form-control date-input"}))
+    before_date = forms.DateField(label=False, required=False, widget=forms.DateInput(
+        attrs={'placeholder': 'date before', "class": "form-control date-input"}))
