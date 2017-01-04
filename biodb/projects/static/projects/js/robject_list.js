a22fbf60c857c6187df5f73174dcbc8e8e03c062
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-04 11:35:33 +0100

    Implement filter dropdown show/hide logic

--- biodb/projects/static/projects/js/robject_list.js
+++ biodb/projects/static/projects/js/robject_list.js
@@ -9,4 +9,28 @@ $(function(){
             $(".select-robject").prop({"checked":false});
         }
     });
+
+    // SEARCH/FILTER TOOLS
+    // searching dropdown content hide/show logic 
+    $(".filter-dropdown-toggle").click(function(event){
+        var content = $(".filter-dropdown-content");
+        
+        if (content.is(":visible")){
+            content.hide();
+        }
+
+        else {
+            content.show();
+            // possible thanks to tabindex tag attr
+            content.focus();
+            content.blur(function(){ 
+                // blure dont concern button click or inputs inside dropdown menu
+                if (!$(".filter-dropdown-toggle:hover").length &&
+                    !$(".filter-dropdown-content *:hover").length){
+                        content.hide();
+                }
+                // TODO: make dropdown-content visible afer click on search bar
+            });
+        }
+    });
 });
