e59e70394489f13ab906e3fad64457a88a172050
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-05 11:06:28 +0100

    Add class name to field in SearchFilterForm

--- biodb/projects/forms.py
+++ biodb/projects/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class SearchFilterForm(forms.Form):
 
     query = forms.CharField(label=False, required=False, widget=forms.TextInput(
-        attrs={'placeholder': 'search phrase','class': 'search-bar'}))
+        attrs={'placeholder': 'search phrase','class': 'form-control search-bar'}))
     after_date = forms.DateField(label=False, required=False, widget=forms.DateInput(
         attrs={'placeholder': 'date after', "class": "form-control date-input"}))
     before_date = forms.DateField(label=False, required=False, widget=forms.DateInput(
