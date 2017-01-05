f49aef5596abb6833c3e8ef7855166ea07b6b867
Adrian <adrian.jasinski@adamed.com.pl>
2017-01-05 14:59:29 +0100

    update

--- biodb/projects/admin.py
+++ biodb/projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Project, RObject, Name
+from .models import Project, RObject, Name
 from guardian.admin import GuardedModelAdmin
 
 # Register your models here.
