a7adc326fa22c01adcd9ca7920199db9c41dfd17
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-10 11:49:46 +0100

    Add views tests

--- biodb/projects/tests/test_views.py
+++ biodb/projects/tests/test_views.py
@@ -1,58 +1,101 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User, Permission
 # from django.contrib.auth
-from projects.models import Project
+from projects.models import Project, RObject, Name
 from django.core.urlresolvers import reverse
 from guardian.shortcuts import assign_perm
+import datetime
 
+
+# TODO: consider if this superclass is a good idea (slow speed down)
 class ProjectsViewsTests(TestCase):
-    
+    ''' Superclass for every test class in this module '''
+
     @classmethod
     def setUpClass(cls):
-        ''' Called once in the begining before any tests. '''
+        ''' Called once when test class is set up. Provide all basic requirements to inteact 
+        with views: create user, activate user, log client in, create project, attach project 
+        permission to user.  '''
 
         super(ProjectsViewsTests, cls).setUpClass()
 
-        # create user 
-        user = User.objects.create_user(username="john", password="johnpassword")
+        # create user
+        cls.user = User.objects.create_user(
+            username="john", password="johnpassword")
 
         # activate
-        user.is_active = True
+        cls.user.is_active = True
 
-        # add permissions
-        permission = Permission.objects.get(codename='can_visit')
-        user.user_permissions.add(permission)
-
-
-        # create projects
-        test_project = Project.objects.create(name="test_project")
-
-        # assign permission to project instances
-        assign_perm("can_visit", user, test_project)
-
-        # log in client 
+        # log client in
         cls.c = Client()
         cls.c.login(username="john", password="johnpassword")
-    
+
+        # create projects
+        cls.project = Project.objects.create(name="test_project")
+
+        # add permissions
+        permission = Permission.objects.get(codename='can_visit_project')
+        cls.user.user_permissions.add(permission)
+
+        # assign permission to project instance
+        assign_perm("can_visit_project", cls.user, cls.project)
+
+
+class ProjectListViewTests(ProjectsViewsTests):
+
     def test_ProjectListView(self):
 
-        resp = self.c.get(reverse("projects:project_list"), follow=True)
+        resp = self.c.get(reverse("projects:project_list"))
 
-        
         self.assertTemplateUsed(resp, 'projects/project_list.html')
         self.assertEqual(resp.status_code, 200)
 
-    # def test_ProjectDetailView(self):
-    #     resp = self.c.get(reverse("projects:project_detail", kwargs={"slug":"test_project"}))
-        
-    #     self.assertTemplateUsed(resp, 'projects/project_detail.html')
-    #     self.assertEqual(resp.status_code, 200)
 
-    # def test_RobjectListView(self):
-    #     resp = self.c.get(reverse("projects:robject_list"))
+class RObjectListView(ProjectsViewsTests):
+    @classmethod
+    def setUpClass(cls):
+        ''' Extend super setUpClass '''
 
-    #     self.assertTemplateUsed(resp, 'projects/robject_list.html')
-    #     self.assertEqual(resp.status_code, 200) 
+        super(RObjectListView, cls).setUpClass()
+
+        # create robjects (2 related to project, 1 unrelated)
+        cls.robject_1 = RObject.objects.create(project=cls.project, create_date=datetime.date(2010, 10, 20))
+        cls.robject_2 = RObject.objects.create(project=cls.project, create_date=datetime.date(2000, 01, 10))
+        cls.robject_3 = RObject.objects.create()
+
+        # create robjects names
+        Name.objects.create(title="robject_1", primary=True,
+                            robject=cls.robject_1)
+        Name.objects.create(title="robject_2", primary=True,
+                            robject=cls.robject_2)
+        Name.objects.create(title="robject_3", primary=True,
+                            robject=cls.robject_3)
+
+    def test_RObjectListView(self):
+        # test get request (get_table_data method)
+        resp = self.c.get(reverse("projects:project_detail",
+                                  kwargs={"project_name": "test_project"}))
+
+        self.assertIn(self.robject_1, resp.context["table"].data)
+        self.assertIn(self.robject_2, resp.context["table"].data)
+        self.assertNotIn(self.robject_3, resp.context["table"].data)
+        self.assertTemplateUsed(resp, 'projects/robject_list.html')
+        self.assertEqual(resp.status_code, 200)
+
+        # test post request
+
+        # test if form_valid render proper template, create new form instace and calls get_table_data()
+        # test if get_table_data() calls SearchMixin.search() and SearchMixin.filter() methods
+        resp = self.c.post(reverse("projects:project_detail", 
+                           kwargs={"project_name": "test_project"}), 
+                           data={"query": "robject", "after_date": "2010-01-01", "before_date": ""})
+
+        self.assertIn(self.robject_1, resp.context["table"].data)
+        self.assertEqual(len(resp.context["table"].data), 1)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'projects/robject_list.html')
+        self.assertFalse(resp.context["form"].is_bound)
+
 
     # def test_RobjectDetailView(self):
     #     pass
@@ -74,5 +117,3 @@ class ProjectsViewsTests(TestCase):
 
     # def test_BioObjDeleteView(self):
     #     pass
-
-
