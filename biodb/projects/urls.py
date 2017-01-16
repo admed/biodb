d152cda842e429b229ff8179adb9e128b24b1182
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-16 16:04:33 +0100

    Add robject_create url schema

--- biodb/projects/urls.py
+++ biodb/projects/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from views import ProjectListView, RObjectListView, ProjectUpdateView, RObjectDeleteView
+from views import ProjectListView, RObjectListView, RObjectDeleteView, RObjectCreateView
 
 app_name = "projects"
 urlpatterns = [
@@ -7,6 +7,5 @@ urlpatterns = [
     url(r'^(?P<project_name>\w+)/$', RObjectListView.as_view(), name="robject_list"),
     url(r"^(?P<project_name>\w+)/(?P<robject_ids>(\d+\+)+\d+)/delete/$", RObjectDeleteView.as_view(), name="robject_delete"),
     url(r'^(?P<project_name>\w+)/(?P<robject_ids>\d+)/delete/$', RObjectDeleteView.as_view(), name="robject_delete"),
-    url(r'^(?P<pk>[0-9]{1,3})/edit/$',
-        ProjectUpdateView.as_view(), name="project_update")
+    url(r'^(?P<project_name>\w+)/create/$', RObjectCreateView.as_view(), name="robject_create")
 ]
