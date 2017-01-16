f36c19509f549ed245b4f5007ba5b4bd5eb7d374
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-16 16:06:43 +0100

    Add view handling robject create using rebar

--- biodb/projects/views.py
+++ biodb/projects/views.py
@@ -2,21 +2,24 @@ from biodb import settings
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from django.views import generic
-from models import Project, RObject
+from models import Project, RObject, Name
 # from guardian.shortcuts import get_objects_for_user
 from guardian.mixins import PermissionRequiredMixin, PermissionListMixin, LoginRequiredMixin
 from biodb import mixins
 from tables import RObjectTable
 from django_tables2 import SingleTableMixin
-from forms import SearchFilterForm
+from forms import SearchFilterForm, BaseNameFormSet
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.http import Http404
 from patches.shortcuts import get_objects_or_404
-
+from django.forms import modelform_factory, modelformset_factory
+from rebar.group import formgroup_factory
 
 # Create your views here.
+
+
 def redirect_to_home(request):
     ''' In case of request '/' redirect to '/projects/' '''
     return redirect(to=settings.HOME_URL)
@@ -103,6 +106,7 @@ class RObjectListView(mixins.ProjectPermissionMixin, PermissionRequiredMixin, Si
             "project_name": self.kwargs["project_name"]
         })
 
+
 class RObjectDeleteView(mixins.ProjectPermissionMixin, PermissionRequiredMixin, mixins.DeleteMultipleMixin, generic.DeleteView):
     ''' View capable to delete more than one Robject! '''
 
@@ -112,8 +116,48 @@ class RObjectDeleteView(mixins.ProjectPermissionMixin, PermissionRequiredMixin, 
     def get_success_url(self):
         return reverse('projects:robject_list', kwargs={"project_name": self.kwargs["project_name"]})
 
-class ProjectUpdateView(PermissionRequiredMixin, generic.UpdateView):
+
+class RObjectCreateView(mixins.ProjectPermissionMixin, PermissionRequiredMixin, generic.FormView):
     permission_required = 'projects.change_project'
     raise_exception = True
-    model = Project
-    fields = ["name", "description"]
+    model = RObject
+    template_name = "projects/robject_create.html"
+
+    def get_form_class(self):
+        # create RObject model form
+        RObjectForm = modelform_factory(RObject, exclude=("project","creator",))
+        # create Name formset
+        NameFormSet = modelformset_factory(
+            Name, exclude=("robject",), formset=BaseNameFormSet, extra=1)
+        # join above forms into one form using rebar's formgroup_factory
+        RObjectFormGroup = formgroup_factory(
+            (
+                (NameFormSet, "name"),
+                (RObjectForm, "robject"),
+            ),
+        )
+        return RObjectFormGroup
+
+    def form_valid(self, form):
+        # create robject from form, but not save it!
+        robject = form.robject.save(commit=False)
+
+        # bound robject with project and User
+        robject.project = self.get_permission_object()
+        robject.creator = self.request.user
+        
+        # now save it
+        robject.save()
+
+        # the same story with names 
+        names = form.name.save(commit=False)
+
+        # iterate over names, bound to robject and save
+        for name in names:
+            name.robject = robject
+            name.save()
+
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('projects:robject_list', kwargs={"project_name": self.kwargs["project_name"]})
