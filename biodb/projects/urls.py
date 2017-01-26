1b6fac7bd40bf3016da799330f78cf794d010265
Mateusz <mateusz.kirmuc@gmail.com>
2017-01-26 13:31:39 +0100

    Modify robject_create url pattern

--- biodb/projects/urls.py
+++ biodb/projects/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^(?P<project_name>\w+)/$', RObjectListView.as_view(), name="robject_list"),
     url(r"^(?P<project_name>\w+)/(?P<robject_ids>(\d+\+)+\d+)/delete/$", RObjectDeleteView.as_view(), name="robject_delete"),
     url(r'^(?P<project_name>\w+)/(?P<robject_ids>\d+)/delete/$', RObjectDeleteView.as_view(), name="robject_delete"),
-    url(r'^(?P<project_name>\w+)/create/$', RObjectCreateView.as_view(), name="robject_create")
+    url(r'^(?P<project_name>\w+)/create/(?P<number_of_name_forms>[1-9]\d*)/$', RObjectCreateView.as_view(), name="robject_create"),
 ]
