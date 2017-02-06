f2b88b3ef8ea45c3eab147c4c2098576b556cee8
Mateusz <mateusz.kirmuc@gmail.com>
2017-02-06 11:22:30 +0100

    Add webpack_loader settings

--- biodb/biodb/settings.py
+++ biodb/biodb/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = (
     'watson',
     'bootstrap3',
     'simple_history',
+    'webpack_loader'
 
 )
 
@@ -150,3 +151,10 @@ SITE_ID = 2
 
 USE_L10N = False 
 
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+    }
+}
