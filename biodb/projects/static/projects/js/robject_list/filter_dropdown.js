dcac333fd5fbc42d3e4705b1f31ebcbc496d7161
mateusz <mateusz@kirmuc.pl>
2017-02-12 18:26:51 +0100

    projects/templates/projects/robject_list.html

--- biodb/projects/static/projects/js/robject_list/filter_dropdown.js
+++ biodb/projects/static/projects/js/robject_list/filter_dropdown.js
@@ -1,6 +1,7 @@
 $(function(){
     // CREATE DATEPICKER AND SET DATE FORMAT
     $(".date-input").datepicker({ 
-        dateFormat: 'dd-mm-yy'   
+        dateFormat: 'dd-mm-yy',
+        onClose: function(d,i) {$(".filter-dropdown-content").focus()}   
     });
 });
